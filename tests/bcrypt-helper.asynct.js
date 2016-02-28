dc8559a51b40553f7f1203a11519bca2fa0e0461
Cyril CHAPON <cyril.chapon@live.com>
2016-02-28 17:22:51 +0100

    Added tests for '$2b' and '$2y' prefixes

--- tests/bcrypt-helper.asynct.js
+++ tests/bcrypt-helper.asynct.js
@@ -23,6 +23,28 @@ exports['should return true for "$2a$10$ySxTfMoVlFnRZ0pmBUwj3.7zjth/LpXg8a2bYWfz
   test.done();
 };
 
+exports['should return true for "$2b$04$2moPs/x/wnCfeQ5pCheMcuSJQ/KYjOZG780UjA/SiR.KsYWNrC7SG"'] = function(test) {
+  var value = '$2b$04$2moPs/x/wnCfeQ5pCheMcuSJQ/KYjOZG780UjA/SiR.KsYWNrC7SG';
+  
+  var actual = bcryptHelper.isHash(value);
+  var expected = true;
+  
+  assert.equal(actual, expected);
+  
+  test.done();
+};
+
+exports['should return true for "$2y$10$HOuBG8tzKLUyk1RK18Xz2uFE76ROdT7w92RSAclL1IoyiS56o70bO"'] = function(test) {
+  var value = '$2y$10$HOuBG8tzKLUyk1RK18Xz2uFE76ROdT7w92RSAclL1IoyiS56o70bO';
+  
+  var actual = bcryptHelper.isHash(value);
+  var expected = true;
+  
+  assert.equal(actual, expected);
+  
+  test.done();
+};
+
 exports['should return false for "test"'] = function(test) {
   var value = 'test';
   
